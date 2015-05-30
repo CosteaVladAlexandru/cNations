f24e05ead50bba5e90485c4f8bbf19d60a608a24
Costea Vlad Alexandru <Costea.Vlad.Alexandru@gmail.com>
2015-05-30 10:11:32 +0200

    added FAC, removed getters/setters

--- cn.py
+++ cn.py
@@ -4,30 +4,21 @@ class player:
     def __init__(self,name=""):
         self.name=name
         self.id=None
+        self.fac=False
 
     def read_from_file(self,f):
         self.name=f.readline().rstrip('\n')
         self.id=int(f.readline().rstrip('\n'))
+        self.fac=f.readline().rstrip('\n')=='True'
 
     def write_to_file(self,f):
         f.write("player\n")
         f.write(self.name+'\n')
         f.write(str(self.id)+'\n')
-
-    def get_name(self):
-        return self.name
-
-    def set_name(self,new_name=""):
-        self.name=new_name
-
-    def get_id(self):
-        return self.id
-
-    def set_id(self,id):
-        self.id=id
+        f.write(str(self.fac)+'\n')
 
     def print_me(self):
-        return self.name+" "+str(self.id)
+        return self.name+" "+str(self.id)+" Fac="+str(self.fac)
 
     def __hash__(self):
         return hash(self.name)
@@ -77,15 +68,13 @@ def get_player(name):
         def __hash__(self):
             return hash(self.value)
         def __eq__(self,other):
-            if self.value.get_name()==other.get_name():
+            if self.value.name==other.name:
                 self.real_value=other
                 return True
             return False
-        def grab_me(self):
-            return self.real_value
     x=grab(player(name))
     if x in players:
-        return x.grab_me()
+        return x.real_value
     return None
 
 #commands
@@ -108,9 +97,9 @@ def add_player():
         print("Player already exists!")
     else:
         x=player(s)
-        while x.get_id()==None:
+        while x.id==None:
             try:
-                x.set_id(int(input("Id:")))
+                x.id=int(input("Id: "))
             except ValueError:
                 print("Id needs to be a number!")
         players.add(x)
@@ -146,15 +135,28 @@ def delete_forever():
 
 def change_id():
     global players
-    x=get_player(input("Name:").lower())
+    x=get_player(input("Name: ").lower())
     if x==None:
         print("Player not found")
-    original_id=x.get_id()
-    while original_id==x.get_id():
+    original_id=x.id
+    while original_id==x.id:
         try:
-            x.set_id(int(input("Id:")))
+            x.id=int(input("Id: "))
         except ValueError:
             print("Id needs to be a number!")
+
+def change_fac():
+    global players
+    x=get_player(input("Name: ").lower())
+    if x==None:
+        print("Player not found")
+    response=input("Does player have FAC (yes/no): ").lower()
+    while response != "yes" and response != "no":
+        response=input("Does player have FAC (yes/no): ").lower()
+    if response=="yes":
+        x.fac=True
+    else:
+        x.fac=False
 
 def main():
     global commands
@@ -168,7 +170,8 @@ def main():
         'check':player_exists,
         'list':list_all,
         'delete':delete_forever,
-        'id':change_id}
+        'id':change_id,
+        'fac':change_fac}
     try:
         read_data()
     except FileNotFoundError:
